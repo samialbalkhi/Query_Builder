306173022be2019de8c72b8e56133ffe631f9866
samialbalkhi <samialbalkhi766@gmail.com>
2023-02-13 10:10:11 +0200

    create posts

---
+++ resources/views/posts/createpost.blade.php
@@ -0,0 +1,7 @@
+
+<h1>create new post</h1>
+<form>
+    <input type="text" name="title" placeholder="Enyet Title"><br><br>
+    <input type="text" name="body " placeholder="Enyet Body"><br><br>
+    <button type="button"> create</button>
+</form>
